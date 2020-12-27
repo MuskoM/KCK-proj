d411d9d3e51658f8182b706722bfff5c7c6f8668
MuskoM <muskomatt@outlook.com>
2020-12-27 18:58:44 +0100

    Added virtual env and testing Tkinter

---
+++ guiApp.py
@@ -0,0 +1,67 @@
+from News import News
+from CovidStatistics import CovidStatistics
+import tkinter as tk
+from tkinter.colorchooser import askcolor
+from tkinter.messagebox import askyesnocancel
+from tkinter.scrolledtext import ScrolledText
+from tkinter.ttk import Progressbar
+
+
+class MainWindow(tk.Frame):
+    news_viewer_window = None;
+    stats_window = None;
+
+    def __init__(self,master):
+        super().__init__(master)
+        self.menu = tk.Menu(self.master)
+        self.initalize_menus()
+
+        self.news_reader = ScrolledText()
+        self.news_reader.pack()
+        self.pack()
+
+    def open_news_viewer_window(self):
+        self.news_viewer_window = tk.Toplevel(self.master)
+        self.news_viewer_window.title("News Viewer")
+        news_reader_fragment = ScrolledText(self.news_viewer_window)
+        tk.Label(self.news_viewer_window, text="Title").pack()
+        article_page_progress = Progressbar(self.news_viewer_window, length=300, mode="determinate")
+        article_page_progress['value'] = 50
+        article_page_progress.pack()
+        news_reader_fragment.insert('1.0', 'oi whats\nthe matter')
+        news_reader_fragment['state'] = 'disabled'
+        news_reader_fragment.pack()
+        tk.Label(self.news_viewer_window, text="This is a covid Stats window").pack()
+
+
+    def open_covid_stats_window(self):
+        self.stats_window = tk.Toplevel(self.master)
+        self.stats_window.title("Covid Stats")
+        tk.Label(self.stats_window, text="This is a covid Stats window").pack()
+
+    def initalize_menus(self):
+        self.master.config(menu=self.menu)
+
+        file_menu = tk.Menu(self.menu)
+        edit_menu = tk.Menu(self.menu)
+        modules_menu = tk.Menu(self.menu)
+
+        self.menu.add_cascade(label="File", menu=file_menu)
+        file_menu.add_command(label="New")
+        file_menu.add_command(label="Save")
+        self.menu.add_cascade(label="Edit", menu=edit_menu)
+        edit_menu.add_command(label="Cut")
+        edit_menu.add_command(label="Copy")
+        edit_menu.add_separator()
+        edit_menu.add_command(label="Settings")
+        self.menu.add_cascade(label="Modules", menu=modules_menu)
+        modules_menu.add_command(label="News Reader", command=self.open_news_viewer_window)
+        modules_menu.add_command(label="Covid Statistics", command=self.open_covid_stats_window)
+
+
+    def print_contents(self,event):
+        if self.contents.get() == "AskQuestion":
+            print("Hi, the current contents is: " + self.contents.get())
+        else:
+            answer = askyesnocancel("Asking", "Yes no dialog")
+            print(answer)
--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from app import MyApp
+import tkinter
+from guiApp import MainWindow
+import tkinter as tk
 
 if __name__ == '__main__':
 
-    App = MyApp()
-    App.run()
+    # App = MyApp()
+    # App.run()
+    root = tk.Tk()
+    app = MainWindow(master=root)
+    app.master.title("My app")
+    app.master.maxsize(200,200)
+    app.mainloop()
 
